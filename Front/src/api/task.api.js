3d554ff5fb7da252aaddf04fa7a781dab2688dde
Alejo <alejoczombos@gmail.com>
2024-01-18 17:23:18 -0300

    Añadir JWT en el header de las peticiones

--- Front/src/api/task.api.js
+++ Front/src/api/task.api.js
@@ -1,13 +1,18 @@
-import axios from 'axios'
+import axios from "axios";
+import { GetToken, GetUserId } from "../service/loginService";
 
 const taskApi = axios.create({
-    baseURL: "http//localhost:8081/api/tasks"
-})
+  baseURL: "http//localhost:8081/api/tasks",
+  headers: { Authorization: `bearer ${GetToken()}` },
+});
 
-export const getAllTask = (userId) => taskApi.get(`/${userId}`)
+export const getAllTask = (userId) => taskApi.get(`/${userId}`);
 
-export const createTask = (task) => taskApi.post("", task)
+export const getTaskById = (taskId) => taskApi.get(`/${taskId}`);
 
-export const updateTask = (task) => taskApi.put("", task)
+export const createTask = (task) =>
+  taskApi.post(`?userId=${GetUserId()}`, task);
 
-export const deleteTask = (taskId) => taskApi.delete(`/${taskId}`)
+export const updateTask = (task) => taskApi.put(`?userId=${GetUserId()}`, task);
+
+export const deleteTask = (taskId) => taskApi.delete(`/${taskId}`);
