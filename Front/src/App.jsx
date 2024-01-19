e619b7988070c032e8204b58090401095e58fb06
Alejo <alejoczombos@gmail.com>
2024-01-19 18:14:54 -0300

    cambios varios

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,40 +1,42 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import {Toaster} from 'react-hot-toast'
-import {LoginProvider} from './Context/login'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { LoginProvider } from "./Context/login";
+import "./App.css";
 
-import {TasksPage} from './pages/TasksPage'
-import {TasksFormPage} from './pages/TasksFormPage'
-import {LoginPage} from './pages/LoginPage'
-import {RegisterPage} from './pages/RegisterPage'
-import {Aside} from './components/Aside'
-import {Navigation} from './components/Navigation'
+import { TasksPage } from "./pages/TasksPage";
+import { TasksFormPage } from "./pages/TasksFormPage";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { Aside } from "./components/Aside";
+import { Navigation } from "./components/Navigation";
 
 function App() {
-
-  return(
+  return (
     <LoginProvider>
       <BrowserRouter>
-        <div id="app" className="relative h-screen w-full p-2 gap-4 bg-zinc-950">
+        <div
+          id="app"
+          className="relative h-screen w-full p-2 gap-4 bg-zinc-950"
+        >
           <aside className="[grid-area:aside] overflow-y-auto bg-zinc-900 rounded-xl border-2 border-zinc-800">
-          <Aside/>
+            <Aside />
           </aside>
-          <main className="[grid-area:main] bg-zinc-900 rounded-xl border-2 border-zinc-800">
-            <Navigation/>
+          <main className="[grid-area:main] bg-zinc-900 rounded-xl border-2 border-zinc-800 overflow-y-auto">
+            <Navigation />
             <Routes>
-              <Route path="/" element={<Navigate to="/tasks"/>}/>
-              <Route path="/tasks" element={<TasksPage/>} />
-              <Route path="/tasks-create" element={<TasksFormPage/>} />
-              <Route path="/tasks/:id" element={<TasksFormPage/>} />
+              <Route path="/" element={<Navigate to="/tasks" />} />
+              <Route path="/tasks" element={<TasksPage />} />
+              <Route path="/tasks-create" element={<TasksFormPage />} />
+              <Route path="/tasks/:id" element={<TasksFormPage />} />
             </Routes>
           </main>
-          <LoginPage/>
-          <RegisterPage/>
-          <Toaster/>
+          <LoginPage />
+          <RegisterPage />
+          <Toaster />
         </div>
       </BrowserRouter>
-      </LoginProvider>
-  )
+    </LoginProvider>
+  );
 }
 
-export default App
+export default App;
--- Front/src/service/loginService.js
+++ Front/src/service/loginService.js
@@ -1,49 +1,47 @@
-import {jwtDecode} from 'jwt-decode'
+import { jwtDecode } from "jwt-decode";
 
 export const SaveUser = (token) => {
-    const decodedToken = jwtDecode(token)
+  const decodedToken = jwtDecode(token);
 
-    const user = {
-        fullname: `${decodedToken.firstname} ${decodedToken.lastname}`,
-        userId: decodedToken.id,
-        JWT: token
-    }
+  const user = {
+    fullname: `${decodedToken.firstname} ${decodedToken.lastname}`,
+    userId: decodedToken.id,
+    JWT: token,
+  };
 
-    window.localStorage.setItem(
-        'TaskManagerUser', JSON.stringify(user)
-        )
-}
+  window.localStorage.setItem("TaskManagerUser", JSON.stringify(user));
+};
 
 export const DeleteUser = () => {
-    window.localStorage.removeItem('TaskManagerUser')
-}
+  window.localStorage.removeItem("TaskManagerUser");
+};
 
 export const CheckIsLogin = () => {
-    const TaskManagerUser = window.localStorage.getItem('TaskManagerUser')
-    const user = JSON.parse(TaskManagerUser)
-    return user ? true : false
-}
+  const TaskManagerUser = window.localStorage.getItem("TaskManagerUser");
+  const user = JSON.parse(TaskManagerUser);
+  return user ? true : false;
+};
 
 export const GetUser = () => {
-    const TaskManagerUser = window.localStorage.getItem('TaskManagerUser')
-    const user = JSON.parse(TaskManagerUser)
-    return user
-}
+  const TaskManagerUser = window.localStorage.getItem("TaskManagerUser");
+  const user = JSON.parse(TaskManagerUser);
+  return user;
+};
 
 export const GetToken = () => {
-    const TaskManagerUser = window.localStorage.getItem('TaskManagerUser')
-    const user = JSON.parse(TaskManagerUser)
-    return user.JWT
-}
+  const TaskManagerUser = window.localStorage.getItem("TaskManagerUser");
+  const user = JSON.parse(TaskManagerUser);
+  return user?.JWT;
+};
 
 export const GetFullname = () => {
-    const TaskManagerUser = window.localStorage.getItem('TaskManagerUser')
-    const user = JSON.parse(TaskManagerUser)
-    return user.fullname
-}
+  const TaskManagerUser = window.localStorage.getItem("TaskManagerUser");
+  const user = JSON.parse(TaskManagerUser);
+  return user.fullname;
+};
 
 export const GetUserId = () => {
-    const TaskManagerUser = window.localStorage.getItem('TaskManagerUser')
-    const user = JSON.parse(TaskManagerUser)
-    return user.userId
-}
+  const TaskManagerUser = window.localStorage.getItem("TaskManagerUser");
+  const user = JSON.parse(TaskManagerUser);
+  return user.userId;
+};
