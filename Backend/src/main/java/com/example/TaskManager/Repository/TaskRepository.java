1b18b2d565fefc94e8c277f79f06b724fb9e3d8d
Alejo <alejoczombos@gmail.com>
2024-01-19 18:09:53 -0300

    Query Methods de filtros de busqueda

--- Backend/src/main/java/com/example/TaskManager/Repository/TaskRepository.java
+++ Backend/src/main/java/com/example/TaskManager/Repository/TaskRepository.java
@@ -4,6 +4,17 @@ import com.example.TaskManager.Entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByUserIdOrderByCreationDateDesc(Long userId);
+
+    List<Task> findAllByFinishedTrue();
+
+    List<Task> findAllByFinishedFalse();
+
+    List<Task> findAllByImportantTrue();
+
 }
