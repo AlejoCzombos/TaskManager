274a4d3101d692e5b71d0dbc3cc29f27440771c6
Alejo <alejoczombos@gmail.com>
2024-01-20 17:13:18 -0300

    Traer todas las tareas desde backend

--- Front/src/pages/TasksPage.jsx
+++ Front/src/pages/TasksPage.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import { TaskCard } from "../components/TaskCard";
 import { TaskCreate } from "../components/TaskCreate";
+import { getAllTasks } from "../api/task.api";
+import { useLogin } from "../Context/login";
 
 export function TasksPage() {
+  const { isLogin } = useLogin();
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function getTasks() {
+      if (isLogin) {
+        const res = await getAllTasks();
+        const data = await res.json();
+        setTasks(data);
+      } else {
+        setTasks([]);
+      }
+    }
+    getTasks();
+  }, []);
+
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-4 m-5">
-      {TasksPage.map((task) => {
-        <TaskCard task={task} />;
+      {tasks.map((task) => {
+        return <TaskCard task={task} key={task.id} />;
       })}
       <TaskCreate />
     </div>
