ff1a7fa2afb14eb91fbfc4918bf963cbb345f384
Alejo <alejoczombos@gmail.com>
2024-01-21 22:46:23 -0300

    cambio de axios a fetch en api auth

--- Front/src/api/auth.api.js
+++ Front/src/api/auth.api.js
@@ -1,17 +1,42 @@
-import axios from 'axios'
-import {SaveUser} from '../service/loginService'
+import { SaveUser } from "../service/loginService";
 
-const authApi = axios.create({
-    baseURL: "http://localhost:8081/api/auth"
-})
+const BASEURL = "http://localhost:8081/api/auth";
 
-export const AuthLogin = async (data, setIsLogin) => { 
-    const res = await authApi.post("/login", data)
-    setIsLogin(true)
-    SaveUser(res.data.token)
-}
+export const AuthLogin = (body) => {
+  const url = `${BASEURL}/login`;
 
-export const AuthRegister = async (data) => {
-    const res = await authApi.post("/register", data)
-    SaveUser(res.data.token)
-}
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+/*export const AuthLogin = async (data, setIsLogin) => {
+  const res = await authApi.post("/login", data);
+  setIsLogin(true);
+  SaveUser(res.data.token);
+};*/
+
+export const AuthRegister = (body) => {
+  const url = `${BASEURL}/register`;
+
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      SaveUser(data.token);
+    })
+    .catch((error) => {
+      console.error("Error during registration:", error);
+      // Handle the error appropriately, e.g., show an error message to the user
+    });
+};
