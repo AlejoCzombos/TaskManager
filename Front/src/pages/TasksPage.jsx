487af4cb9492f57cbfccc0b0d6a3a3485b465805
Alejo <alejoczombos@gmail.com>
2024-01-24 21:54:15 -0300

    Filtros de lista de tareas

--- Front/src/pages/TasksPage.jsx
+++ Front/src/pages/TasksPage.jsx
@@ -1,25 +1,51 @@
 import { useEffect, useState } from "react";
 import { TaskCard } from "../components/TaskCard";
 import { TaskCreate } from "../components/TaskCreate";
-import { getAllTasks } from "../api/task.api";
+import {
+  getAllTasks,
+  getCompletedTasks,
+  getImportantTasks,
+  getIncompletedTasks,
+} from "../api/task.api";
 import { useLogin } from "../Context/login";
+import { useParams } from "react-router-dom";
 
 export function TasksPage() {
   const { isLogin } = useLogin();
   const [tasks, setTasks] = useState([]);
 
+  const params = useParams();
+
   useEffect(() => {
     async function getTasks() {
       if (isLogin) {
-        const res = await getAllTasks();
+        let res;
+        switch (params.filter) {
+          case "all":
+            res = await getAllTasks();
+            break;
+          case "important":
+            res = await getImportantTasks();
+            break;
+          case "completed":
+            res = await getCompletedTasks();
+            break;
+          case "incompleted":
+            res = await getIncompletedTasks();
+            break;
+          default:
+            res = await getAllTasks();
+        }
+
         const data = await res.json();
         setTasks(data);
       } else {
         setTasks([]);
       }
     }
+
     getTasks();
-  }, []);
+  }, [isLogin, params.filter]);
 
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-4 m-5">
