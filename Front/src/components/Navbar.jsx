0bb23717bb09254ead61ac84049b8d33b24937c5
Alejo <alejoczombos@gmail.com>
2024-01-24 21:51:35 -0300

    cambio de botones a las rutas nuevas

--- Front/src/components/Navbar.jsx
+++ Front/src/components/Navbar.jsx
@@ -1,88 +1,80 @@
-import { useState } from 'react';
-import {SideMenuItem} from './SideMenuItem'
+import { useState } from "react";
+import { SideMenuItem } from "./SideMenuItem";
 
 export function Navbar() {
   return (
     <div>
-            <SideMenuItem
-            text="Todas las Tareas"
-            to="/tasks">
-                <HomeIcon className="size-6"/>
-            </SideMenuItem>
+      <SideMenuItem text="Todas las Tareas" to="/tasks/all">
+        <HomeIcon className="size-6" />
+      </SideMenuItem>
 
-            <SideMenuItem
-            text="Importante!"
-            to="/important">
-                <ImportantIcon className="size-6"/>
-            </SideMenuItem>
+      <SideMenuItem text="Importante!" to="/tasks/important">
+        <ImportantIcon className="size-6" />
+      </SideMenuItem>
 
-            <SideMenuItem 
-            text="Completado!"
-            to="/completed">
-                <CheckIcon className="size-6"/>
-            </SideMenuItem>
+      <SideMenuItem text="Completado!" to="/tasks/completed">
+        <CheckIcon className="size-6" />
+      </SideMenuItem>
 
-            <SideMenuItem
-            text="Por hacer"
-            to="/todo">
-                <ListIcon className="size-6"/>
-            </SideMenuItem>
-
-        </div>
-  )
+      <SideMenuItem text="Por hacer" to="/tasks/incompleted">
+        <ListIcon className="size-6" />
+      </SideMenuItem>
+    </div>
+  );
 }
-
 
 function HomeIcon(props) {
   return (
     <svg
-    {...props}
-    xmlns="http://www.w3.org/2000/svg" 
-    height="16" 
-    width="18"
-    viewBox="0 0 576 512">
-    <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="18"
+      viewBox="0 0 576 512"
+    >
+      <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
     </svg>
-  )
+  );
 }
 
 function ImportantIcon(props) {
-return (
-  <svg 
-  {...props}
-  xmlns="http://www.w3.org/2000/svg" 
-  height="16" 
-  width="16" 
-  viewBox="0 0 512 512">
-  <path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z"/>
-  </svg>
-)
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="16"
+      viewBox="0 0 512 512"
+    >
+      <path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z" />
+    </svg>
+  );
 }
-
 
 function CheckIcon(props) {
-return (
-  <svg 
-  {...props}
-  xmlns="http://www.w3.org/2000/svg"
-   height="16" 
-   width="14" 
-   viewBox="0 0 448 512">
-      <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-  </svg>
-)
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="14"
+      viewBox="0 0 448 512"
+    >
+      <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+    </svg>
+  );
 }
 
-
 function ListIcon(props) {
-return (
-  <svg 
-  {...props}
-  xmlns="http://www.w3.org/2000/svg"
-  height="16" 
-  width="12" 
-  viewBox="0 0 384 512">
-      <path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM305 273L177 401c-9.4 9.4-24.6 9.4-33.9 0L79 337c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L271 239c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
-  </svg>
-)
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="12"
+      viewBox="0 0 384 512"
+    >
+      <path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM305 273L177 401c-9.4 9.4-24.6 9.4-33.9 0L79 337c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L271 239c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+    </svg>
+  );
 }
