a61a95d169e42a7928859b0b60fa306ba3bc5231
Alejo <alejoczombos@gmail.com>
2024-01-25 23:49:20 -0300

    Refactor Aside component and add navigation

--- Front/src/components/Aside.jsx
+++ Front/src/components/Aside.jsx
@@ -1,65 +1,60 @@
-import { useContext } from 'react'
-import {LoginContext} from '../Context/login'
-import {DeleteUser} from '../service/loginService'
+import { useContext } from "react";
+import { LoginContext } from "../Context/login";
+import { DeleteUser } from "../service/loginService";
 
-import {toast} from 'react-hot-toast'
-import {Navbar} from './Navbar'
-import {SideMenuItem} from './SideMenuItem'
-import {Profile} from './Profile'
+import { toast } from "react-hot-toast";
+import { Navbar } from "./Navbar";
+import { SideMenuItem } from "./SideMenuItem";
+import { Profile } from "./Profile";
+import { useNavigate } from "react-router-dom";
 
 export function Aside() {
+  const { isLogin, setIsLogin, setIsOpenLogin } = useContext(LoginContext);
 
-  const {isLogin, setIsLogin, isOpenLogin, setIsOpenLogin} = useContext(LoginContext)
-  
-  const handleClickLogin = () => {
-    setIsOpenLogin(isLogin ? false : true)
+  const navigate = useNavigate();
+
+  const handleClickLogin = async () => {
+    setIsOpenLogin(isLogin ? false : true);
     if (isLogin) {
-      DeleteUser()
-      setIsLogin(false)
-      toast.success("Sesión Cerrada correctamente")
+      await DeleteUser();
+      setIsLogin(false);
+      toast.success("Sesión cerrada");
+      await navigate("/");
     }
-  }
+  };
 
   return (
     <div className="flex flex-col justify-between h-full">
-        
-        <Profile/>
-        
-        <Navbar/>
-        
-        <SideMenuItem
-          text={ isLogin ? "Cerrar Sesión" : "Iniciar Sesión"}
-          onClick={handleClickLogin}
-        >
-            <OutIcon className="size-8"/>
-        </SideMenuItem>
+      <Profile />
+
+      <Navbar />
+
+      <SideMenuItem
+        text={isLogin ? "Cerrar Sesión" : "Iniciar Sesión"}
+        onClick={handleClickLogin}
+      >
+        {isLogin ? (
+          <OutIcon className="size-8" />
+        ) : (
+          <InIcon className="size-8" />
+        )}
+      </SideMenuItem>
     </div>
-  )
+  );
 }
-
-
 
 function OutIcon(props) {
   return (
-    <svg 
-    {...props}
-    xmlns="http://www.w3.org/2000/svg" 
-    height="16" 
-    width="16" 
-    viewBox="0 0 512 512">
-        <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/>
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
     </svg>
-  )
+  );
 }
 
-function PlusIcon(props) {
+function InIcon(props) {
   return (
-    <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    height="16" 
-    width="14" 
-    viewBox="0 0 448 512">
-        <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M352 96l64 0c17.7 0 32 14.3 32 32l0 256c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0c53 0 96-43 96-96l0-256c0-53-43-96-96-96l-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-9.4 182.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L242.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
     </svg>
-  )
+  );
 }
