26b1a3cb333d7b36c3605b2997c25b532896d7df
Alejo <alejoczombos@gmail.com>
2024-01-25 23:49:33 -0300

    Refactor Navigation and Profile components

--- Front/src/components/Navigation.jsx
+++ Front/src/components/Navigation.jsx
@@ -1,33 +1,36 @@
-import {Link} from 'react-router-dom'
-import '../App.css'
+import { Link } from "react-router-dom";
+import "../App.css";
 
 export function Navigation() {
-    return (
-        <div className='flex justify-between items-center m-5 mb-6'>
-            <Link to="/tasks" className=' text-white font-bold text-3xl navigation'>
-                <h1>Tareas</h1>
-            </Link>
-            <Link to="/tasks-create">
-                <PlusIcon className="
+  return (
+    <div className="flex justify-between items-center m-5 mb-6">
+      <Link to="/" className=" text-white font-bold text-3xl navigation">
+        <h1>Tareas</h1>
+      </Link>
+      <Link to="/tasks-create">
+        <PlusIcon
+          className="
                 fill-zinc-500 bg-zinc-800/60
                 border-2 border-zinc-800
                 rounded-full
                 transition hover:scale-125
-                size-9 p-1.5"/>
-            </Link>
-        </div>
-    )
+                size-9 p-1.5"
+        />
+      </Link>
+    </div>
+  );
 }
 
 function PlusIcon(props) {
-    return (
-      <svg 
+  return (
+    <svg
       {...props}
-      xmlns="http://www.w3.org/2000/svg" 
-      height="16" 
-      width="14" 
-      viewBox="0 0 448 512">
-          <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
-      </svg>
-    )
-  }
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="14"
+      viewBox="0 0 448 512"
+    >
+      <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+    </svg>
+  );
+}
--- Front/src/components/Profile.jsx
+++ Front/src/components/Profile.jsx
@@ -1,10 +1,15 @@
 export function Profile(props) {
   return (
-    <div className="flex text-white text-xl items-center space-x-4 p-5 text-wrap" {...props}>
-        <img src="https://drive.google.com/uc?export=view&id=0B6wwyazyzml-OGQ3VUo0Z2thdmc"
+    <div
+      className="flex text-white text-xl items-center justify-center space-x-4 p-5 text-wrap"
+      {...props}
+    >
+      <img
+        src="https://drive.google.com/uc?export=view&id=0B6wwyazyzml-OGQ3VUo0Z2thdmc"
         alt="profile photo"
-        className="size-16 rounded-full"/>
-        <span className="font-bold">Tiger Woods</span>
+        className="size-16 rounded-full"
+      />
+      <span className="font-bold">Alejo Czombos</span>
     </div>
-  )
+  );
 }
