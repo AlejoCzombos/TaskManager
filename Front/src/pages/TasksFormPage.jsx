e7f43bcaeaddbb7b579c5609e57c226b0aa282f1
Alejo <alejoczombos@gmail.com>
2024-01-27 21:50:58 -0300

    Titulo al formulario de creación y estilos focus

--- Front/src/pages/TasksFormPage.jsx
+++ Front/src/pages/TasksFormPage.jsx
@@ -61,23 +61,27 @@ export function TasksFormPage() {
   return (
     <div className="max-w-md mx-auto">
       <form onSubmit={onSubmit}>
+        <h1 className="text-center text-white font-bold text-3xl mb-4">
+          Creación de Tareas
+        </h1>
         <div className="mb-5">
           <label
             htmlFor="title"
             className="block mb-2 text-sm font-medium text-white"
           >
-            Título:
+            Titulo de ejemplo
           </label>
           <input
+            autoFocus
             type="title"
             id="title"
-            className=" border-2 text-sm rounded-xl block w-full p-2.5 bg-zinc-700 border-zinc-600 placeholder-gray-400 text-white focus:ring-green-500/90 focus:border-green-500/90"
+            className=" border-2 text-sm rounded-xl block w-full p-2.5 bg-zinc-700 border-zinc-600 placeholder-gray-400 text-white focus:ring-green-500/90 focus:border-green-500/90 focus:outline-none"
             placeholder="Título"
             {...register("title", {
               required: "El título es requerido",
               maxLength: {
                 value: 50,
-                message: "El título puede tener un maximo de 50 caracteres",
+                message: "El título puede tener un máximo de 50 caracteres",
               },
               minLength: {
                 value: 5,
@@ -100,13 +104,13 @@ export function TasksFormPage() {
           <textarea
             id="description"
             rows="4"
-            className="block p-2.5 w-full min-h-16 max-h-32 text-sm rounded-xl border-2 bg-zinc-700 border-zinc-600 placeholder-gray-400 text-white focus:ring-green-500/90 focus:border-green-500/90"
+            className="block p-2.5 w-full min-h-16 max-h-32 text-sm rounded-xl border-2 bg-zinc-700 border-zinc-600 placeholder-gray-400 text-white focus:ring-green-500/90 focus:border-green-500/90 focus:outline-none"
             placeholder="Descripción"
             {...register("description", {
               maxLength: {
                 value: 250,
                 message:
-                  "La descripción puede tener un maximo de 50 caracteres",
+                  "La descripción puede tener un máximo de 50 caracteres",
               },
               minLength: {
                 value: 5,
@@ -131,7 +135,7 @@ export function TasksFormPage() {
           <input
             id="important"
             type="checkbox"
-            className="w-4 h-4 text-blue-600 rounded focus:ring-blue-600 ring-offset-zinc-600 focus:ring-offset-zinc-600 focus:ring-2 bg-zinc-700 border-zinc-600"
+            className="w-4 h-4 rounded focus:ring-green-500/90 focus:border-green-500/90 focus:outline-none ring-offset-zinc-600 focus:ring-offset-zinc-600 focus:ring-2 bg-zinc-700 border-zinc-600"
             {...register("important")}
           />
         </div>
@@ -146,7 +150,7 @@ export function TasksFormPage() {
           <input
             id="finished"
             type="checkbox"
-            className="w-4 h-4 text-blue-600 rounded focus:ring-blue-600 ring-offset-zinc-600 focus:ring-offset-zinc-600 focus:ring-2 bg-zinc-700 border-zinc-600"
+            className="w-4 h-4 rounded focus:ring-green-500/90 focus:border-green-500/90 focus:outline-none ring-offset-zinc-600 focus:ring-offset-zinc-600 focus:ring-2 bg-zinc-700 border-zinc-600"
             {...register("finished")}
           />
         </div>
@@ -156,7 +160,7 @@ export function TasksFormPage() {
         </button>
       </form>
       {params.id && (
-        <button className="mt-3 flex justify-end cursor-pointer bg-red-500/90 text-white font-semibold hover:bg-red-400/90 p-2 px-3.5 rounded-xl">
+        <button className="mt-3 cursor-pointer w-full bg-red-500/90 text-white font-semibold hover:bg-red-400/90 p-2 px-3.5 rounded-xl">
           Eliminar tarea
         </button>
       )}
