03f7386d34fd45449b9c54f335373980a02a0c1d
Alejo <alejoczombos@gmail.com>
2024-01-27 21:50:16 -0300

    agregar foto perfil

--- Front/src/components/Profile.jsx
+++ Front/src/components/Profile.jsx
@@ -1,13 +1,13 @@
 export function Profile(props) {
   return (
     <div
-      className="flex text-white text-xl items-center justify-center space-x-4 p-5 text-wrap"
+      className="flex text-white text-xl items-center justify-center space-x-4 p-5 text-wrap hover:bg-zinc-800 cursor-pointer"
       {...props}
     >
       <img
-        src="https://drive.google.com/uc?export=view&id=0B6wwyazyzml-OGQ3VUo0Z2thdmc"
+        src="Profile.webp"
         alt="profile photo"
-        className="size-16 rounded-full"
+        className="size-16 rounded-full object-cover object-top"
       />
       <span className="font-bold">Alejo Czombos</span>
     </div>
