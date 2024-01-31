86d1b404604ccb5fb0713e077ddf1a2c8ecaad9c
Alejo <alejoczombos@gmail.com>
2024-01-31 19:24:01 -0300

    Link a perfil de Github

--- Front/src/components/Profile.jsx
+++ Front/src/components/Profile.jsx
@@ -1,15 +1,16 @@
 export function Profile(props) {
   return (
-    <div
+    <a
+      href="https://github.com/AlejoCzombos"
       className="flex text-white text-xl items-center justify-center space-x-4 p-5 text-wrap hover:bg-zinc-800 cursor-pointer"
       {...props}
     >
       <img
-        src="Profile.webp"
+        src="/Profile.webp"
         alt="profile photo"
         className="size-16 rounded-full object-cover object-top"
       />
       <span className="font-bold">Alejo Czombos</span>
-    </div>
+    </a>
   );
 }
