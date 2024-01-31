f5797ea91a18e21215227f27ff1e996835401eed
Alejo <alejoczombos@gmail.com>
2024-01-31 20:56:10 -0300

    Refactorización del fetch de register

--- Front/src/api/auth.api.js
+++ Front/src/api/auth.api.js
@@ -14,29 +14,14 @@ export const AuthLogin = (body) => {
   });
 };
 
-/*export const AuthLogin = async (data, setIsLogin) => {
-  const res = await authApi.post("/login", data);
-  setIsLogin(true);
-  SaveUser(res.data.token);
-};*/
-
 export const AuthRegister = (body) => {
   const url = `${BASEURL}/register`;
 
-  fetch(url, {
+  return fetch(url, {
     method: "POST",
     body: JSON.stringify(body),
     headers: {
       "Content-Type": "application/json",
     },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      SaveUser(data.token);
-    })
-    .catch((error) => {
-      console.error("Error during registration:", error);
-      // Handle the error appropriately, e.g., show an error message to the user
-    });
+  });
 };
