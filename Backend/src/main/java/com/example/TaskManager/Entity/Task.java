36b819aa925b41cda653db3f50fcddcbdc55de64
Alejo <alejoczombos@gmail.com>
2024-02-07 23:14:53 -0300

    Agregar anotación en la clase Task para facilitar pruebas unitarias

--- Backend/src/main/java/com/example/TaskManager/Entity/Task.java
+++ Backend/src/main/java/com/example/TaskManager/Entity/Task.java
@@ -3,6 +3,7 @@ package com.example.TaskManager.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +14,7 @@ import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Data
 @Entity
 @Table(name = "task")
